e91e5cb6815ce875c96cb929df24d7a2c1019f1b
mattrafalko <mattrafalko@gmail.com>
2020-05-23 20:59:04 -0500

    pointing stripe api to vercel now demo page

--- pages/api/createSession.js
+++ pages/api/createSession.js
@@ -16,8 +16,8 @@ export default async (req, res) => {
       customer_email: data.email,
       shipping_address_collection: { allowed_countries: ['US'] }, // ['US'], //{ ...data.shipping.address },
       mode: 'payment',
-      success_url: 'https://sewchallenged-nxgey5oq2.now.sh/checkoutsuccess',
-      cancel_url: 'https://sewchallenged-nxgey5oq2.now.sh/checkout',
+      success_url: 'https://sewchallenged.mattrafalko.now.sh/checkoutsuccess',
+      cancel_url: 'https://sewchallenged.mattrafalko.now.sh/checkout',
     });
 
     res.status(200).json(session);
